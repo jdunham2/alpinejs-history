a45b95b1999cd6470473ca3034af1df9d5850dfa
jdunham2 <jeshuad@gmail.com>
2024-07-02 11:36:59 -0400

    Add history plugin src

---
+++ index.js
@@ -0,0 +1,34 @@
+Alpine.magic('history', (el, { interceptor }) => {
+    return interceptor((initialValue, getter, setter, path, key) => {
+        let lookupKey = path;
+        
+        const url = new URL(window.location.href);
+        const params = new URLSearchParams(url.search);
+
+        let initial = params.has(lookupKey)
+            ? params.get(lookupKey)
+            : initialValue
+        
+        // console.log(initial, initialValue);
+
+        setter(initial)
+
+        Alpine.effect(() => {
+            let value = getter()
+            const url = new URL(window.location.href);
+            const params = new URLSearchParams(url.search);
+            params.set(lookupKey, value)
+            window.history.replaceState({}, '', `${location.pathname}?${params}`);
+            // console.log('new value for', lookupKey, params.get(lookupKey));
+            // console.log(window.location.href)
+
+            setter(value)
+        })
+
+        return initial
+    }, func => {
+        func.alwaysShow = () => { alwaysShow = true; return func }
+        func.usePush = () => { usePush = true; return func }
+        func.as = key => { alias = key; return func }
+    })
+})
