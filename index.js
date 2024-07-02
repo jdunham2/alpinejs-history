8e9adeddbcea685fc8a53d5ee4aa8dc9bea3feb5
jdunham2 <jeshuad@gmail.com>
2024-07-02 11:51:35 -0400

    move magic inside aplint init listener

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-Alpine.magic('history', (el, { interceptor }) => {
-    return interceptor((initialValue, getter, setter, path, key) => {
-        let lookupKey = path;
-        
-        const url = new URL(window.location.href);
-        const params = new URLSearchParams(url.search);
-
-        let initial = params.has(lookupKey)
-            ? params.get(lookupKey)
-            : initialValue
-        
-        // console.log(initial, initialValue);
-
-        setter(initial)
-
-        Alpine.effect(() => {
-            let value = getter()
+document.addEventListener('alpine:init', () => {
+    Alpine.magic('history', (el, { interceptor }) => {
+        return interceptor((initialValue, getter, setter, path, key) => {
+            let lookupKey = path;
+            
             const url = new URL(window.location.href);
             const params = new URLSearchParams(url.search);
-            params.set(lookupKey, value)
-            window.history.replaceState({}, '', `${location.pathname}?${params}`);
-            // console.log('new value for', lookupKey, params.get(lookupKey));
-            // console.log(window.location.href)
-
-            setter(value)
+    
+            let initial = params.has(lookupKey)
+                ? params.get(lookupKey)
+                : initialValue
+            
+            // console.log(initial, initialValue);
+    
+            setter(initial)
+    
+            Alpine.effect(() => {
+                let value = getter()
+                const url = new URL(window.location.href);
+                const params = new URLSearchParams(url.search);
+                params.set(lookupKey, value)
+                window.history.replaceState({}, '', `${location.pathname}?${params}`);
+                // console.log('new value for', lookupKey, params.get(lookupKey));
+                // console.log(window.location.href)
+    
+                setter(value)
+            })
+    
+            return initial
+        }, func => {
+            func.alwaysShow = () => { alwaysShow = true; return func }
+            func.usePush = () => { usePush = true; return func }
+            func.as = key => { alias = key; return func }
         })
-
-        return initial
-    }, func => {
-        func.alwaysShow = () => { alwaysShow = true; return func }
-        func.usePush = () => { usePush = true; return func }
-        func.as = key => { alias = key; return func }
     })
 })
